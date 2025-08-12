9582664866680e49388662fb1410260933b3607b
JacksonVarela <jackson_varela_work@outlook.com>
2025-08-12 16:23:05 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,1 +1,16 @@
+// src/firebase.js
+import { initializeApp, getApps } from "firebase/app";
+
+export function initFirebaseApp() {
+  const cfg = {
+    apiKey: import.meta.env.VITE_FB_API_KEY,
+    authDomain: import.meta.env.VITE_FB_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FB_PROJECT_ID,
+    appId: import.meta.env.VITE_FB_APP_ID,
+    // (optional if you want)
+    // storageBucket: import.meta.env.VITE_FB_STORAGE_BUCKET,
+    // messagingSenderId: import.meta.env.VITE_FB_MESSAGING_SENDER_ID,
+  };
+  if (!getApps().length) initializeApp(cfg);
+}
 

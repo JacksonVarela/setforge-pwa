0ea4afba6986781067f7b55cdb7f8c0613f0772b
JacksonVarela <jackson_varela_work@outlook.com>
2025-08-17 15:06:33 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,22 +2,22 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import ErrorBoundary from "./components/ErrorBoundary.jsx";
 
-import { registerSW } from "virtual:pwa-register";
+// Register SW (safe to import, does nothing in dev)
+import "virtual:pwa-register";
 
-registerSW({
-  onNeedRefresh() {
-    if (confirm("A new version is available. Refresh now?")) {
-      window.location.reload();
-    }
-  },
-  onOfflineReady() {
-    // offline ready (noop)
-  }
-});
+function Root() {
+  // simple global flag for debugging in console
+  if (typeof window !== "undefined") window.SF_DEBUG = true;
+  return (
+    <React.StrictMode>
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </React.StrictMode>
+  );
+}
 
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const mount = document.getElementById("root");
+createRoot(mount).render(<Root />);

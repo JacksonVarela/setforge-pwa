ee40401aa7fd7683642b89ed7c5c42e86b5ab29c
JacksonVarela <jackson_varela_work@outlook.com>
2025-08-26 20:18:21 -0500

    Delete src/auth.js

--- src/auth.js
+++
@@ -1,8 +0,0 @@
-// src/auth.js
-import { getAuth } from "firebase/auth";
-import { initFirebaseApp } from "./firebase";
-
-// Create ONE auth instance tied to the ONE app
-const app = initFirebaseApp();
-export const auth = getAuth(app);
-export default auth;
